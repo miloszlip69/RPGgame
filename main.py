ac2ce4bd57a308efabd236a12531eb20aa2a1502
miloszlip <miloszlip2009@gmail.com>
2022-09-13 16:55:49 +0200

    Player test 1

--- main.py
+++ main.py
@@ -2,17 +2,51 @@ import pygame
 
 pygame.init()
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.speedx = None
+        self.image = pygame.Surface([50, 50])
+        self.image.fill((255, 255, 255))
+
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -8
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 8
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+
 WIDTH = 1000
 HEIGHT = 800
 
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 run = True
-
 clock = pygame.time.Clock()
 
+all_sprites = pygame.sprite.Group(Player())
+
 while run:
+    clock.tick(60)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    screen.fill((255, 255, 255))
+    all_sprites.draw(screen)
+    all_sprites.update()
+
     pygame.display.flip()
 pygame.quit()
