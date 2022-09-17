5df0354a4305dce8cd935e17a6af94dd8e7c6455
miloszlip <miloszlip2009@gmail.com>
2022-09-17 10:22:41 +0200

    Platforms

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ class Player(pygame.sprite.Sprite):
 
         self.speedx = 0
         self.speedy = 0
+        self.onGround = True
 
         self.image = player_img[0]
         self.image = pygame.transform.scale(self.image, (14 * 4, 25 * 4))
@@ -27,18 +28,21 @@ class Player(pygame.sprite.Sprite):
         self.angle = None
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.rect.bottom = HEIGHT
 
     def update(self):
         self.speedx = 0
-        self.speedy += 1
+        if not self.speedy > 4:
+            self.speedy += 1
 
         keystate = pygame.key.get_pressed()
         if keystate[pygame.K_a]:
-            self.speedx = -8
+            self.speedx = -6
         if keystate[pygame.K_d]:
-            self.speedx = 8
-        if keystate[pygame.K_SPACE]:
-            self.speedy = -10
+            self.speedx = 6
+        if keystate[pygame.K_SPACE] and self.onGround:
+            self.speedy = -20
+            self.onGround = False
 
         self.rect.x += self.speedx
         self.rect.y += self.speedy
@@ -48,10 +52,38 @@ class Player(pygame.sprite.Sprite):
         elif self.rect.left < 0:
             self.rect.left = 0
         if self.rect.bottom > HEIGHT:
+            self.onGround = True
             self.rect.bottom = HEIGHT
             self.speedy = 0
         elif self.rect.top < 0:
             self.rect.top = 0
+
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = stone_img
+        self.image = pygame.transform.scale(self.image, (16 * 2, 16 * 2))
+
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def update(self):
+        collide = self.rect.colliderect(player.rect)
+
+        if collide:
+            if player.speedy < 0:
+                player.speedy = 3
+            if self.rect.top + 7 > player.rect.bottom > self.rect.top:
+                player.rect.bottom = self.rect.top + 1
+                player.speedy = 0
+                player.onGround = True
+            else:
+                if self.rect.left < player.rect.right < self.rect.left + 7:
+                    player.rect.right = self.rect.left - 1
+                elif self.rect.right > player.rect.left > self.rect.right - 7:
+                    player.rect.left = self.rect.right + 1
 
 
 WIDTH = 1000
@@ -65,8 +97,12 @@ clock = pygame.time.Clock()
 # Img's
 
 player_img = [pygame.image.load(os.path.join(img_dir, "pKnight.png")).convert()]
+stone_img = pygame.image.load(os.path.join(img_dir, "stone.png")).convert()
 
-all_sprites = pygame.sprite.Group(Player())
+player = Player()
+all_sprites = pygame.sprite.Group(player)
+platforms = pygame.sprite.Group(Platform(200, HEIGHT - 50),
+                                Platform(350, HEIGHT - 120))
 
 while run:
     clock.tick(60)
@@ -77,6 +113,8 @@ while run:
 
     screen.fill(WHITE)
     all_sprites.draw(screen)
+    platforms.draw(screen)
+    platforms.update()
     all_sprites.update()
 
     pygame.display.flip()
